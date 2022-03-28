a0af553658bc7df88451787bc5b0d2135757a0fe
stephanieesquire <stephanieesquiremarco@gmail.com>
2022-03-28 15:41:56 -0300

    product form

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,83 +7,83 @@ function Sidebar() {
     <>
       <nav
         id="sidebarMenu"
-        class="sidebar d-lg-block bg-gray-800 text-white collapse"
+        className="sidebar d-lg-block bg-gray-800 text-white collapse"
         data-simplebar
       >
-        <div class="sidebar-inner px-4 pt-3">
-          <ul class="nav flex-column pt-3 pt-md-0">
+        <div className="sidebar-inner px-4 pt-3">
+          <ul className="nav flex-column pt-3 pt-md-0">
             <li
               role="separator"
-              class="dropdown-divider mt-4 mb-3 border-gray-700"
+              className="dropdown-divider mt-4 mb-3 border-gray-700"
             ></li>
-            <li class="nav-item">
+            <li className="nav-item">
               <a
                 href="https://demo.themesberg.com/volt-pro/pages/calendar.html"
                 target="_blank"
-                class="nav-link d-flex justify-content-between"
+                className="nav-link d-flex justify-content-between"
               >
                 <span>
-                  <span class="sidebar-text">Categories</span>
+                  <span className="sidebar-text">Categories</span>
                 </span>
               </a>
             </li>
             <li
               role="separator"
-              class="dropdown-divider mt-4 mb-3 border-gray-700"
+              className="dropdown-divider mt-4 mb-3 border-gray-700"
             ></li>
-            <li class="nav-item">
+            <li className="nav-item">
               <Link
                 to={"/products"}
                 className={"nav-link d-flex justify-content-between"}
               >
                 <span>
-                  <span class="sidebar-text">Products</span>
+                  <span className="sidebar-text">Products</span>
                 </span>
               </Link>
             </li>
             <li
               role="separator"
-              class="dropdown-divider mt-4 mb-3 border-gray-700"
+              className="dropdown-divider mt-4 mb-3 border-gray-700"
             ></li>
-            <li class="nav-item">
+            <li className="nav-item">
               <span
-                class="nav-link  collapsed  d-flex justify-content-between align-items-center"
+                className="nav-link  collapsed  d-flex justify-content-between align-items-center"
                 data-bs-toggle="collapse"
                 data-bs-target="#submenu-app"
               >
                 <span>
-                  <span class="sidebar-text">Administrators</span>
+                  <span className="sidebar-text">Administrators</span>
                 </span>
               </span>
             </li>
             <li
               role="separator"
-              class="dropdown-divider mt-4 mb-3 border-gray-700"
+              className="dropdown-divider mt-4 mb-3 border-gray-700"
             ></li>
-            <li class="nav-item">
+            <li className="nav-item">
               <span
-                class="nav-link  collapsed  d-flex justify-content-between align-items-center"
+                className="nav-link  collapsed  d-flex justify-content-between align-items-center"
                 data-bs-toggle="collapse"
                 data-bs-target="#submenu-pages"
               >
                 <span>
-                  <span class="sidebar-text">Orders</span>
+                  <span className="sidebar-text">Orders</span>
                 </span>
               </span>
             </li>
 
-            <li class="nav-item">
+            <li className="nav-item">
               <a
                 id="btn-logout"
                 href="../../pages/upgrade-to-pro.html"
-                class="btn d-flex align-items-center justify-content-center btn-upgrade-pro"
+                className="btn d-flex align-items-center justify-content-center btn-upgrade-pro"
               >
                 <span>Logout</span>
               </a>
             </li>
             <li
               role="separator"
-              class="dropdown-divider mt-4 mb-3 border-gray-700"
+              className="dropdown-divider mt-4 mb-3 border-gray-700"
             ></li>
           </ul>
         </div>
--- src/pages/ProductForm/ProductFrom.jsx
+++ src/pages/ProductForm/ProductFrom.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function ProductFrom() {
+  const location = useLocation();
+
   return (
     <>
       <main>
@@ -13,58 +16,90 @@ function ProductFrom() {
               <div className="col-12 d-flex align-items-center justify-content-center">
                 <div className="bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500">
                   <div className="text-center text-md-center mb-4 mt-md-0">
-                    <h1 className="mb-0 h3">Login</h1>
+                    {location.pathname === "/create-product" && (
+                      <h1 className="mb-0 h3">New product</h1>
+                    )}
+
+                    {location.pathname === "/update-product" && (
+                      <h1 className="mb-0 h3">Name product</h1>
+                    )}
                   </div>
+
                   <form action="#" className="mt-4 text-start">
                     <div className="form-group mb-4">
-                      <label htmlFor="email">Email</label>
+                      <label htmlFor="name">Name</label>
                       <div className="input-group">
-                        <span className="input-group-text" id="basic-addon1">
-                          <svg
-                            className="icon icon-xs text-gray-600"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                          </svg>
-                        </span>
                         <input
-                          type="email"
+                          type="text"
                           className="form-control"
-                          id="email"
+                          id="name"
+                          name="name"
                           autoFocus
                           required
                         />
                       </div>
                     </div>
-
-                    <div className="form-group">
-                      <div className="form-group mb-4">
-                        <label htmlFor="password">Password</label>
-                        <div className="input-group">
-                          <span className="input-group-text" id="basic-addon2">
-                            <svg
-                              className="icon icon-xs text-gray-600"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                fillRule="evenodd"
-                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                clipRule="evenodd"
-                              ></path>
-                            </svg>
-                          </span>
-                          <input
-                            type="password"
-                            className="form-control"
-                            id="password"
-                            required
-                          />
-                        </div>
+                    <div className="form-group mb-4">
+                      <div className="mb-3">
+                        <label htmlFor="disabledTextInput">Slug</label>
+                        <input
+                          type="text"
+                          id="disabledTextInput"
+                          className="form-control"
+                          disabled
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group mb-4">
+                      <div className="my-4">
+                        <label htmlFor="textarea">Description</label>
+                        <textarea
+                          className="form-control"
+                          id="textarea"
+                          rows="4"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="form-group mb-4">
+                      <label htmlFor="price">Price</label>
+                      <div className="input-group">
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="price"
+                          name="price"
+                          autoFocus
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group mb-4">
+                      <label htmlFor="stock">Stock</label>
+                      <div className="input-group">
+                        <input
+                          type="number"
+                          className="form-control"
+                          id="stock"
+                          name="stock"
+                          autoFocus
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group mb-4">
+                      <div className="form-check">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          value=""
+                          id="defaultCheck10"
+                        />
+                        <label
+                          className="form-check-label"
+                          htmlFor="defaultCheck10"
+                        >
+                          Featured
+                        </label>
                       </div>
                     </div>
                     <div className="d-grid">
